9ea761909789552b76f681a9fef9049193aec844
Ümit Aşan <umitasan@Umit-MBP.home>
2024-10-28 10:49:37 +0300

    genel degisiklik oncesi birtakim guncelleme

--- src/models.py
+++ src/models.py
@@ -1,6 +1,12 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, text
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional, Annotated
+import enum
+import datetime
+
+
+
 
 
 
@@ -11,6 +17,31 @@ class WorkersOrm(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
+
+
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    compensation: Mapped[Optional[int]]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelte="CASCADE"))
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"),
+                                                           onupdate= datetime.datetime.utcnow)
+
+
+
+
+
 
 
 
